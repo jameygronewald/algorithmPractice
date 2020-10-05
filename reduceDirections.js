fc15ba4c4bea354f1b407cd3ba51310793edeb51
Jamey Gronewald <jrgronewald@gmail.com>
2020-10-05 09:17:51 -0700

    codewars reduce solution

---
+++ reduceDirections.js
@@ -0,0 +1,26 @@
+// In ["NORTH", "SOUTH", "EAST", "WEST"], the direction "NORTH" + "SOUTH" is going north and coming back right away.
+
+// The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other, therefore, the final result is [] (nil in Clojure).
+
+// In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], "NORTH" and "SOUTH" are not directly opposite but they become directly opposite after the reduction of "EAST" and "WEST" so the whole path is reducible to ["WEST", "WEST"].
+
+// Task
+// Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+const dirReduc = arr => {
+  let i = 0;
+  while (i < arr.length) {
+    if (
+      (arr[i] === "SOUTH" && arr[i + 1] === "NORTH") ||
+      (arr[i] === "NORTH" && arr[i + 1] === "SOUTH") ||
+      (arr[i] === "WEST" && arr[i + 1] === "EAST") ||
+      (arr[i] === "EAST" && arr[i + 1] === "WEST")
+    ) {
+      arr.splice(i, 2);
+      i--;
+    } else {
+      i++;
+    }
+  }
+  return arr;
+};
