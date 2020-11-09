6491a591253cc35805c6f63a379f98c58a1bbe48
Jamey Gronewald <jrgronewald@gmail.com>
2020-11-08 18:43:57 -0800

    permutation algorithm

---
+++ permutations.js
@@ -0,0 +1,35 @@
+// In this kata you have to create all permutations of an input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
+
+// Examples:
+
+// permutations('a'); // ['a']
+// permutations('ab'); // ['ab', 'ba']
+// permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+
+const permutations = string => {
+    const array = [];
+    if (string.length === 1) {
+      array.push(string);
+      return array;
+    }
+    for (let i = 0; i < string.length; i++) {
+      let selectedChar = string[i];
+      let remainingCharacters = string.slice(0, i) + string.slice(i + 1, string.length);
+      
+      if (string.indexOf(selectedChar) != i) {
+        continue;
+      }
+      
+      for (let perm of permutations(remainingCharacters)) {
+        array.push(selectedChar + perm)
+      }
+      
+    }
+    
+    return array
+  }
+
+
+console.log(permutations('a'));
+console.log(permutations('ab'));
+console.log(permutations('aabb'));
