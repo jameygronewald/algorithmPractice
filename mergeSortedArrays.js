68dd6ff29b5752c1b51b03f6331e9aa35c166433
Jamey Gronewald <jrgronewald@gmail.com>
2020-12-20 09:48:32 -0800

    merge sorted arrays alg done

---
+++ mergeSortedArrays.js
@@ -0,0 +1,43 @@
+// Write code to merge two sorted arrays into a new sorted array
+
+const mergeSortedArrays = (arr1, arr2) => {
+  if (arr1.length === 0) return arr2;
+  if (arr2.length === 0) return arr1;
+  let newArray = [];
+  let startIndex = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    let j = startIndex;
+    while (j < arr2.length) {
+      if (arr2[j] > arr1[i]) {
+        newArray.push(arr1[i]);
+        break;
+      } else if (arr2[j] === arr1[i]) {
+        newArray.push(arr2[j]);
+        newArray.push(arr1[i]);
+        startIndex++;
+        break;
+      } else {
+        newArray.push(arr2[j]);
+        startIndex++;
+        j++;
+      }
+    }
+    if (j >= arr2.length) {
+      newArray = [...newArray, ...arr1.slice(i)];
+      return newArray;
+    }
+  }
+  if (arr2[startIndex]) {
+    newArray = [...newArray, ...arr2.slice(startIndex)];
+    return newArray;
+  }
+  return newArray;
+};
+
+console.log(mergeSortedArrays([12, 13, 20, 51], [8, 14, 40, 41, 43, 50]));
+console.log(
+  mergeSortedArrays(
+    [1, 5, 23, 63, 143, 213, 634, 4443, 19840],
+    [8, 14, 40, 41, 43, 50]
+  )
+);
